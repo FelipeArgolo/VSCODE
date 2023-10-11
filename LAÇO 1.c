5497baf089065d5a00e0d45aac5f2da05a525758
Felipe <143637558+FelipeArgolo@users.noreply.github.com>
2023-10-11 17:01:42 -0300

    Add files via upload

---
+++ LAÇO 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+
+int i;
+
+for ( i = 1; i <= 15 ; i++)
+{
+    if (i % 2 == 1)
+    {
+        printf("Numeros impares: %d \n", i);
+    }
+    
+}
+
+}
